432622d5a55db6f077d1ec79802bec0068a40ce0
XiaoxuanHEI <cherilyn1011@gmail.com>
2020-02-22 14:36:51 +0100

    admin

--- resources/Views/Deps/Admin/Admin.js
+++ resources/Views/Deps/Admin/Admin.js
@@ -2,3 +2,38 @@
 "use strict";
 
 //Admin exclusive code
+window.onload = function () {
+  $(document).on('click', '#addCompte', function () {
+
+  });
+
+  $(document).on('click', '#delCompte', function () {
+
+  });
+
+  $(document).on('click', '#addSkill', function () {
+    var skill = document.getElementById("existingSkill");
+    var skillInput = document.getElementById("newSkill").value;
+    skill.options.add(new Option(skillInput,skillInput));
+    document.getElementById("newSkill").value = "";
+  });
+
+  $(document).on('click', '#delSkill', function () {
+    var skill = document.getElementById("existingSkill");
+    var index = skill.selectedIndex;
+    skill.options.remove(index);
+  });
+
+  $(document).on('click', '#addCat', function () {
+    var cat = document.getElementById("existingCat");
+    var catInput = document.getElementById("newCategory").value;
+    cat.options.add(new Option(catInput,catInput));
+    document.getElementById("newCategory").value = "";
+  });
+
+  $(document).on('click', '#delCat', function () {
+    var cat = document.getElementById("existingCat");
+    var index = cat.selectedIndex;
+    cat.options.remove(index);
+  });
+}
