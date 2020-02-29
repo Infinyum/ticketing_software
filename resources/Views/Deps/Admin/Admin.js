bab7e77bd2ea86bd42ae9e5794b07c9490a39759
XiaoxuanHEI <cherilyn1011@gmail.com>
2020-02-29 21:14:11 +0100

    modify admin.js

--- resources/Views/Deps/Admin/Admin.js
+++ resources/Views/Deps/Admin/Admin.js
@@ -1,10 +1,27 @@
 //We use JavaScript's stict mode to ensure a high JavaScript code quality
 "use strict";
+var host = document.URL;
 
 //Admin exclusive code
 window.onload = function () {
   $(document).on('click', '#addCompte', function () {
 
+    $.ajax({
+        type: "POST",
+        url: host+"/addcompte",
+        dataType:"json",    //what we send
+        contentType:"application/json", //what we expect as the response
+        data:JSON.stringify({myAttributeName:value}),
+        beforeSend:function(xhr){
+
+        },
+        complete:function(xhr){
+
+        },
+        success:function(element){
+            $(".display").html("Ajouté avec succès!");
+        }
+    });
   });
 
   $(document).on('click', '#delCompte', function () {
@@ -15,6 +32,22 @@ window.onload = function () {
     var skill = document.getElementById("existingSkill");
     var skillInput = document.getElementById("newSkill").value;
     skill.options.add(new Option(skillInput,skillInput));
+    $.ajax({
+        type: "POST",
+        url: host+"/addcompetence",
+        dataType:"json",    //what we send
+        contentType:"application/json", //what we expect as the response
+        data:JSON.stringify({nom:skillInput}),
+        beforeSend:function(xhr){
+           // alert("En cours d'ajout");
+        },
+        complete:function(xhr){
+          // alert("Ajout termine");
+        },
+        success:function(element){
+          alert("Ajouté avec succès!");
+        }
+    });
     document.getElementById("newSkill").value = "";
   });
 
@@ -22,12 +55,44 @@ window.onload = function () {
     var skill = document.getElementById("existingSkill");
     var index = skill.selectedIndex;
     skill.options.remove(index);
+    $.ajax({
+        type: "POST",
+        url: host+"/removecompetence",
+        dataType:"json",    //what we send
+        contentType:"application/json", //what we expect as the response
+        data:JSON.stringify({nom:skillInput}),
+        beforeSend:function(xhr){
+
+        },
+        complete:function(xhr){
+
+        },
+        success:function(element){
+          alert("Supprimé avec succès!");
+        }
+    });
   });
 
   $(document).on('click', '#addCat', function () {
     var cat = document.getElementById("existingCat");
     var catInput = document.getElementById("newCategory").value;
     cat.options.add(new Option(catInput,catInput));
+    $.ajax({
+        type: "POST",
+        url: host+"/addcategorie",
+        dataType:"json",    //what we send
+        contentType:"application/json", //what we expect as the response
+        data:JSON.stringify({nom:skillInput}),
+        beforeSend:function(xhr){
+
+        },
+        complete:function(xhr){
+
+        },
+        success:function(element){
+          alert("Ajouté avec succès!");
+        }
+    });
     document.getElementById("newCategory").value = "";
   });
 
@@ -35,5 +100,21 @@ window.onload = function () {
     var cat = document.getElementById("existingCat");
     var index = cat.selectedIndex;
     cat.options.remove(index);
+    $.ajax({
+        type: "POST",
+        url: host+"/removecategorie",
+        dataType:"json",    //what we send
+        contentType:"application/json", //what we expect as the response
+        data:JSON.stringify({nom:skillInput}),
+        beforeSend:function(xhr){
+
+        },
+        complete:function(xhr){
+
+        },
+        success:function(element){
+          alert("Supprimé avec succès!");
+        }
+    });
   });
 }
